b8483a77846a2a4e5811465515eaca3c4d2ecf26
Daniel Sironen <dsironen@hotmail.com>
2015-12-03 16:41:57 -0500

    Ver 1

---
+++ ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="font-family: Arial, Helvetica, sans-serif">
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>Simple Calculator</h1>
+    
+    </div>
+        <p>
+            First Value:&nbsp;
+            <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Second Value:&nbsp; <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="additionButton" runat="server" OnClick="additionButton_Click" Text="+" />
+&nbsp;
+            <asp:Button ID="subtractionButton" runat="server" OnClick="subtractionButton_Click" Text="-" />
+&nbsp;
+            <asp:Button ID="multiplicationButton" runat="server" OnClick="multiplicationButton_Click" Text="*" />
+&nbsp;
+            <asp:Button ID="divisionButton" runat="server" OnClick="divisionButton_Click" Text="/" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server" BackColor="#66CCFF" Font-Bold="True" Font-Size="Large"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void additionButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstTextBox.Text;
+            int f = int.Parse(firstValue);
+
+            string secondValue = secondTextBox.Text;
+            int s = int.Parse(secondValue);
+
+            int r = f + s;
+             
+
+            resultLabel.Text = r.ToString();
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstTextBox.Text;
+            int f = int.Parse(firstValue);
+
+            string secondValue = secondTextBox.Text;
+            int s = int.Parse(secondValue);
+
+            int r = f - s;
+
+            resultLabel.Text = r.ToString();
+        }
+
+        protected void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstTextBox.Text;
+            int f = int.Parse(firstValue);
+
+            string secondValue = secondTextBox.Text;
+            int s = int.Parse(secondValue);
+
+            int r = f * s;
+
+            resultLabel.Text = r.ToString();
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstTextBox.Text;
+            double f = double.Parse(firstValue);
+
+            string secondValue = secondTextBox.Text;
+            double s = double.Parse(secondValue);
+
+            double r = f / s;
+
+            resultLabel.Text = r.ToString();
+        }
+    }
+}
